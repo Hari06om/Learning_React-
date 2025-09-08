7a2b67c090e646d007f5eee2e26dce2ae62875ae
Hari06om <hariohmverma96@gmail.com>
2025-09-08 20:13:05 +0530

    in the last type script use normal code syantax so in this code i want to use for loop in the script

--- customReact/costomreact.js
+++ customReact/costomreact.js
@@ -1,12 +1,23 @@
+// function customRender(reactElement, container) {
+//     const domElement = document.createElement(reactElement.type); // Create a DOM element of the specified type
+//     domElement.innerHTML = reactElement.children; // Set the inner HTML of the DOM element to the children of the react element
+//     domElement.setAttribute('href', reactElement.props.href); // Set the href attribute of the DOM element
+//     domElement.setAttribute('target', reactElement.props.target); // Set the target attribute of the DOM element
+//     container.appendChild(domElement); // Append the DOM element to the container
+// } // This is a function to render a react element into a container
+// // This is a react element object
 function customRender(reactElement, container) {
-    const domElement = document.createElement(reactElement.type); // Create a DOM element of the specified type
-    domElement.innerHTML = reactElement.children; // Set the inner HTML of the DOM element to the children of the react element
-    domElement.setAttribute('href', reactElement.props.href); // Set the href attribute of the DOM element
-    domElement.setAttribute('target', reactElement.props.target); // Set the target attribute of the DOM element
-    container.appendChild(domElement); // Append the DOM element to the container
-} // This is a function to render a react element into a container
-// This is a react element object
+    const domElement = document.createElement(reactElement.type)
 
+    domElement.innerHTML = reactElement.children
+
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+
+    container.appendChild(domElement)
+}
 
 const reactElement = {
     type: 'a',
@@ -14,7 +25,7 @@ const reactElement = {
         href: 'https://www.google.com',
         target: '_blank'
     },
-    children: 'Click Me to visit Google'
+    children: 'Click Me to visit chatpgt'
 }
 
 const mainContainer = document.querySelector('#root')
