3996ab9fd9395fb625f9b95407aa2b36c1281ce1
Hari06om <hariohmverma96@gmail.com>
2025-11-19 22:02:45 +0530

    make a todo list

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './App.css'
+import {TodoProvider} from "./Contexts"
+
 
 function App() {
+  const[todos , setTodos] = useState([])
+  const addTodo= (todo) => {
+    setTodos((prev) =>[{id: Date.now(),...todo}, ...prev])
+  }
+  
+  const updateTodo = (id,todo) => {
+     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id===id ? todo : prev )))
+  }
+
   return (
- <div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+    <TodoProvider value={{todos,appTodo,deleteTodo,updateTodo,deleteTodo,toggleComplete}} > 
+    <div className=" bg-gradient-to-r from-[#016B61] via-blue-700 to-blue-300 min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white  bg-gradient-to-r from-green-700 via-yellow-600 to-pink-400">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
@@ -13,6 +26,7 @@ function App() {
                     </div>
                 </div>
             </div>
+      </TodoProvider>
   )
 }
 
