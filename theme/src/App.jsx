ffc72587a928f932b751ba30ccf7c840df28158f
Hari06om <hariohmverma96@gmail.com>
2025-11-30 16:25:22 +0530

    add file

--- theme/src/App.jsx
+++ theme/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { ThemeProvider } from './context/theme'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import ThemeBtn from './Components/ThemeBtn'
+import Card from './Components/card'
 
 function App() {
   const [themeMode,setThemeMode] = useState("light")
@@ -28,7 +30,7 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                  <ThemeBtn />
+                  <ThemeBtn/>
               </div>
 
               <div className="w-full max-w-sm mx-auto">
