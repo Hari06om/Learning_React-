e6491603a29f25f0bfff183e47d7780b6f9f5402
Hari06om <hariohmverma96@gmail.com>
2026-02-02 20:52:29 +0530

    Counter useing UseState in react

--- ContextApi/minicontext/src/context/UserContextProvider.jsx
+++ ContextApi/minicontext/src/context/UserContextProvider.jsx
@@ -8,7 +8,7 @@ const UserContextProvider = ({children}) => {
         <UserContext.Provider value={{user,setUser}}>
         {children}
         </UserContext.Provider>
-    )
+    )   
 }
 
 export default UserContextProvider
